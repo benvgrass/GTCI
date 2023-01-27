f67317bf28686e960d4ced9e2f86e6bbf8e03a6b
Ben Grass <bvgrass@outlook.com>
2023-01-27 12:13:37 -0500

    added driver code

--- two-pointers/validpalindrome.go
+++ two-pointers/validpalindrome.go
@@ -1,5 +1,10 @@
 package two_pointers
 
+import (
+	"fmt"
+	"strings"
+)
+
 func isPalindrome(inputString string) bool {
 	left := 0
 	right := len(inputString) - 1
@@ -14,6 +19,14 @@ func isPalindrome(inputString string) bool {
 	return true
 }
 
+// driver code from educative.io course to test isPalindrome function
 func main() {
-
+	str := []string{"RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"}
+	for i, s := range str {
+		fmt.Printf("Test Case # %d\n", i+1)
+		fmt.Printf("%s\n", strings.Repeat("-", 100))
+		fmt.Printf("\tThe input string is '%s' and the length of the string is %d.\n", s, len(s))
+		fmt.Printf("\n\tIs it a palindrome?.....%v\n", isPalindrome(s))
+		fmt.Printf("%s\n", strings.Repeat("-", 100))
+	}
 }
