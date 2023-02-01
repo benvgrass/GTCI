89b21c51201b0ff70419bf26b13127e3f1e382f1
Ben Grass <bvgrass@outlook.com>
2023-02-01 18:01:26 -0500

    changing structure

---
+++ two-pointers/sumofthree/sumofthree.go
@@ -0,0 +1,15 @@
+package main
+
+// insert starter code
+func findSumOfThree(nums []int, target int) bool {
+
+	return true
+}
+
+func quickSort(arr []int, i int, j int) []int {
+	return []int{}
+}
+
+func main() {
+
+}
