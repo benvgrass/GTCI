379a161478337f2623b270d57a35e55b69876361
Ben Grass <bvgrass@outlook.com>
2023-02-09 23:13:36 -0500

    wrote setup and tests for vp2

---
+++ two-pointers/validpalindrome2/vp2.go
@@ -0,0 +1,6 @@
+package main
+
+func validPalindrome(s string) bool {
+	// start with original vp algo and then change
+	return false
+}
---
+++ two-pointers/validpalindrome2/vp2_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestValidPalindrome(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"madame", true},
+		{"dead", true},
+		{"abca", false},
+		{"tebbem", false},
+		{"eeccccbebaeeabebccceea", false},
+		{"ognfjhgbjhzkqhzadmgqbwqsktzqwjexqvzjsopolnmvnymbbzoofzbbmynvmnloposjzvqxejwqztksqwbqgmdazhqkzhjbghjfno", false},
+	}
+
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("ValidPalindromeII.%d", i), func(t *testing.T) {
+			res := validPalindrome(tc.input)
+			if res != tc.want {
+				t.Fatalf("actual %v; want %v", res, tc.want)
+			} else {
+				t.Logf("Pass!")
+			}
+
+		})
+	}
+
+}
