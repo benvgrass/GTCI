a20c5e06825cb79b7d1dc0f1dd118bdbe0204f70
Ben Grass <bvgrass@outlook.com>
2023-02-12 16:01:37 -0500

    wrote formula looking for logic error

--- two-pointers/validpalindrome2/vp2.go
+++ two-pointers/validpalindrome2/vp2.go
@@ -3,11 +3,17 @@ package main
 func validPalindrome(s string) bool {
 	left := 0
 	right := len(s) - 1
+	skipped := false
 
 	for left <= right {
-		if s[left] != s[right] {
-			// skip and check
+
+		if !skipped && s[left] != s[right] {
+			skipped = true
 		}
+		if skipped && s[left+1] != s[right] && s[left] != s[right-1] {
+			return false
+		}
+
 		left++
 		right--
 	}
