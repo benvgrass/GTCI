e81cfb45c4a7d940c5bb2c864d1e8eea0ac392c5
Ben Grass <bvgrass@outlook.com>
2023-02-13 16:48:48 -0500

    implemented test for detect cycle

--- fast-slow/llcycle/llcycle_test.go
+++ fast-slow/llcycle/llcycle_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type DetectCycleTestPair struct {
 	input    *EduLinkedListNode
@@ -8,7 +11,24 @@ type DetectCycleTestPair struct {
 }
 
 func TestDetectCycle(t *testing.T) {
+	tests := []DetectCycleTestPair{
+		createTestPair([]int{2, 4, 6, 8, 10}, 2),
+		createTestPair([]int{1, 3, 5, 7, 9}, -1),
+		createTestPair([]int{1, 2, 3, 4, 5}, 3),
+		createTestPair([]int{0, 2, 3, 5, 6}, -1),
+		createTestPair([]int{3, 6, 9, 10, 11}, 0),
+	}
 
+	for i, testCase := range tests {
+		t.Run(fmt.Sprintf("DetectCycle-%d", i), func(t *testing.T) {
+			actual := detectCycle(testCase.input)
+			if actual != testCase.expected {
+				t.Fatalf("expected: %v, actual %v", testCase.expected, actual)
+			} else {
+				t.Logf("Pass!")
+			}
+		})
+	}
 }
 
 func createTestPair(list []int, cycle int) DetectCycleTestPair {
