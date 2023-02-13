f063d598061f645d30764d8180c7d30e2edd3ec9
Ben Grass <bvgrass@outlook.com>
2023-02-13 17:58:14 -0500

    implemented get middle node

--- fast-slow/midll/midll.go
+++ fast-slow/midll/midll.go
@@ -2,6 +2,12 @@ package main
 
 // getMiddleNode function to find the middle node of the linked list
 func getMiddleNode(head *EduLinkedListNode) *EduLinkedListNode {
-	// TODO
-	return nil
+	slow := head
+	fast := head
+
+	for fast != nil && fast.next != nil {
+		slow = slow.next
+		fast = fast.next.next
+	}
+	return slow
 }
