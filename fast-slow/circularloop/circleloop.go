a6420e3c4793e9ece0806b7fede965d301f15c23
Ben Grass <bvgrass@outlook.com>
2023-02-27 22:14:07 -0500

    wrote algorithm for circle loop

--- fast-slow/circularloop/circleloop.go
+++ fast-slow/circularloop/circleloop.go
@@ -1,6 +1,43 @@
 package main
 
 func circularArrayLoop(arr []int) bool {
-	// TODO
+	getCircleIdx := func(currIdx, delta int) int {
+		l := len(arr)
+		newIdx := (currIdx + delta) % l
+		if newIdx < 0 {
+			newIdx += l
+		}
+		return newIdx
+	}
+	nextIdx := func(idx int) int {
+		return getCircleIdx(idx, arr[idx])
+	}
+
+	visited := make(map[int]bool)
+
+	for idx := range arr {
+
+		if !visited[idx] {
+			circleContinues := func(currIdx int) bool {
+				return (arr[idx] > 0) == (arr[currIdx] > 0)
+			}
+
+			slow := idx
+			fast := idx
+
+			for circleContinues(fast) && circleContinues(nextIdx(fast)) {
+				slow = nextIdx(slow)
+				visited[fast] = true
+				fast = nextIdx(fast)
+				visited[fast] = true
+				fast = nextIdx(fast)
+				if slow == fast {
+					return true
+				}
+			}
+
+		}
+
+	}
 	return false
 }
