dbed8cf162b7bfce77dc57225c77a33827b79aa5
Ben Grass <bvgrass@outlook.com>
2023-03-26 20:53:16 -0400

    optimized using sliding window technique

--- 03-sliding-window/02-find-max-sw/max_window.py
+++ 03-sliding-window/02-find-max-sw/max_window.py
@@ -1,38 +1,34 @@
 def find_max_sliding_window(nums, w):
-    window = OrderedList(nums[:w])
+    window = ArrayWindowSlider(w, nums)
     max_windows = [window.max()]
     i = 0
     while i + w < len(nums):
-        max_windows.append(window.slide_and_max(nums[i], nums[i+w]))
+        max_windows.append(window.slide_max(i+w, i+1))
         i += 1
     return max_windows
 
 
-class OrderedList:
-    def __init__(self, nums):
+class ArrayWindowSlider:
+    def __init__(self, w, nums):
         self.window = []
-        for num in nums:
-            self.__insert(num)
+        self.num_ref = nums
+        for idx in range(w):
+            self.__insert(idx)
 
-    def __insert(self, num):
-        inserted = False
+    def __insert(self, idx):
+        num = self.num_ref[idx]
         i = 0
-        while i < len(self.window) and not inserted:
-            if num > self.window[i]:
-                self.window.insert(i, num)
-                inserted = True
+        while i < len(self.window) and num < self.num_ref[self.window[i]]:
             i += 1
-        if not inserted:
-            self.window.append(num)
+        del self.window[i:]
+        self.window.append(idx)
 
-    def __remove(self, num):
-        assert num in self.window
-        self.window.remove(num)
+    def slide_max(self, new_idx, least_index):
+        self.__insert(new_idx)
+        return self.max(least_index)
 
-    def slide_and_max(self, old_num, new_num):
-        self.__remove(old_num)
-        self.__insert(new_num)
-        return self.max()
+    def max(self, least_index=0):
+        while self.window[0] < least_index:
+            del self.window[0]
+        return self.num_ref[self.window[0]]
 
-    def max(self):
-        return self.window[0]
