21aa020f5a0a1a3a05df306812f0de66899f11da
Ben Grass <bvgrass@outlook.com>
2023-08-10 16:05:46 -0400

    made code more concise

--- 03-sliding-window/04-min-substring/min_substring.py
+++ 03-sliding-window/04-min-substring/min_substring.py
@@ -1,13 +1,8 @@
 def min_window(s, t):
-    min_window = ""
+    min_window_str = ""
+    min_win_len = float("inf")
     w_store = {}  # store for window
     t_store = {}  # store for t
-
-    def is_substring():
-        for char in t_store:
-            if w_store[char] < t_store[char]:
-                return False
-        return True
 
     # add characters for s and t stores, t store
     for c in t:
@@ -18,28 +13,24 @@ def min_window(s, t):
 
     w_start = 0
     w_end = 0
-    incr = True
-    while w_end < len(s) or is_substring():
-        if incr:
-            c = s[w_end]
-            w_end += 1
-            if c in w_store:
-                w_store[c] += 1
-                if is_substring():
-                    if len(min_window) == 0 or w_end - w_start< len(min_window):
-                        min_window = s[w_start: w_end]
-                    incr = False
-        else:
+
+    while w_end < len(s):
+        c = s[w_end]
+        w_end += 1
+        if c in w_store:
+            w_store[c] += 1
+
+        while all(w_store[char] >= t_store[char] for char in t_store):
+            if w_end - w_start < min_win_len:
+                min_window_str = s[w_start: w_end]
+                min_win_len = w_end - w_start
+
             c = s[w_start]
             w_start += 1
             if c in w_store:
                 w_store[c] -= 1
-            if is_substring():
-                if w_end - w_start < len(min_window):
-                    min_window = s[w_start: w_end]
-            else:
-                incr = True
-    return min_window
+
+    return min_window_str
 
 
 
