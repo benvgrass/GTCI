24937bf02a3726b48f7a0de9547bb0b54f68c87d
Ben Grass <bvgrass@outlook.com>
2023-08-10 17:04:03 -0400

    implemented sliding window algorithm

--- 03-sliding-window/05-longest-substr/longest_substring.py
+++ 03-sliding-window/05-longest-substr/longest_substring.py
@@ -1,5 +1,19 @@
 def find_longest_substring(input_str):
+    chars = {}
+    longest = -1
+    start = 0
+    end = 0
+    len_input = len(input_str)
 
-   # Replace this placeholder return statement with your code
+    while end < len_input and len_input - start > longest:
+        c = input_str[end]
+        if c in chars:
+            new_start = chars[c] + 1
+            if new_start > start:
+                start = new_start
+        chars[c] = end
+        end += 1
+        if end - start > longest:
+            longest = end - start
 
-   return -1
+    return longest
