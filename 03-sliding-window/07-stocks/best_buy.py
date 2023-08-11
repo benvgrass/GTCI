662383a78f745ad46f021510910b0a7f99fc6af7
Ben Grass <bvgrass@outlook.com>
2023-08-11 13:40:47 -0400

    wrote algorithm with sliding window approach

--- 03-sliding-window/07-stocks/best_buy.py
+++ 03-sliding-window/07-stocks/best_buy.py
@@ -1,5 +1,11 @@
 def max_profit(stock_prices):
+    profit_max = 0
+    buy = 0
 
-    # Replace this placeholder return statement with your code
+    for sell in range(1, len(stock_prices)):
+        if stock_prices[sell] < stock_prices[buy]:
+            buy = sell
+        else:
+            profit_max = max(profit_max, stock_prices[sell] - stock_prices[buy])
 
-    return -1
+    return profit_max
