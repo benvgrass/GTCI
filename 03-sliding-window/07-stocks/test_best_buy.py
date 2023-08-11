5f81615187a372f0bb37b7ef45664b354e266cdc
Ben Grass <bvgrass@outlook.com>
2023-08-11 13:28:27 -0400

    implemented tests

--- 03-sliding-window/07-stocks/test_best_buy.py
+++ 03-sliding-window/07-stocks/test_best_buy.py
@@ -1,2 +1,13 @@
-def test_max_profit():
-    assert False
+import pytest
+from best_buy import max_profit
+
+
+@pytest.mark.parametrize("prices, expected", [
+    ([1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9], 9),
+    ([7, 1, 5, 3, 6, 4], 5),
+    ([7, 6, 4, 3, 1], 0),
+    ([2, 6, 8, 7, 8, 7, 9, 4, 1, 2, 4, 5, 8], 7),
+    ([1, 4, 2], 1)
+])
+def test_max_profit(prices, expected):
+    assert max_profit(prices) == expected
