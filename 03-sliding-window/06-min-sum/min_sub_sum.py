f81a33a0bbbaab3d5e1cf58dff9b4b226fa98309
Ben Grass <bvgrass@outlook.com>
2023-08-11 13:11:26 -0400

    implemented algorithm with sliding window

--- 03-sliding-window/06-min-sum/min_sub_sum.py
+++ 03-sliding-window/06-min-sum/min_sub_sum.py
@@ -1,4 +1,21 @@
 def min_sub_array_len(target, nums):
-    # Replace this placeholder return statement with your code
+    min_len = float("inf")
+    win_start = 0
+    win_end = 0
+    nums_len = len(nums)
+    win_sum = 0
 
-    return -1
+    while win_end < nums_len:
+        win_sum += nums[win_end]
+        win_end += 1
+
+        while win_sum >= target:
+            win_len = win_end - win_start
+            if win_len < min_len:
+                min_len = win_len
+            win_sum -= nums[win_start]
+            win_start += 1
+
+    if min_len == float("inf"):
+        return 0
+    return min_len
