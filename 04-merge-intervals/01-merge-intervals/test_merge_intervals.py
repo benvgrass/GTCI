8bb4fe589ef784e8c6ba94d2ffe4b5f9a13b0b8b
Ben Grass <bvgrass@outlook.com>
2023-08-11 21:36:07 -0400

    fixed test cases to use Interval class

--- 04-merge-intervals/01-merge-intervals/test_merge_intervals.py
+++ 04-merge-intervals/01-merge-intervals/test_merge_intervals.py
@@ -1,13 +1,14 @@
 import pytest
 from merge_intervals import merge_intervals
+from merge_intervals import Interval
 
 
 @pytest.mark.parametrize("intervals, expected", [
-    ([[1, 5], [3, 7], [4, 6]], [[1, 7]]),
-    ([[1, 5], [4, 6], [6, 8], [11, 15]], [[1, 8], [11, 15]]),
-    ([[1, 5]], [[1, 5]]),
-    ([[1, 9], [3, 8], [4, 4]], [[1, 9]]),
-    ([[1, 2], [3, 4], [8, 8]], [[1, 2], [3, 4], [8, 8]])
+    ([Interval(1, 5), Interval(3, 7), Interval(4, 6)], [Interval(1, 7)]),
+    ([Interval(1, 5), Interval(4, 6), Interval(6, 8), Interval(11, 15)], [Interval(1, 8), [11, 15]]),
+    ([Interval(1, 5)], [Interval(1, 5)]),
+    ([Interval(1, 9), Interval(3, 8), Interval(4, 4)], [Interval(1, 9)]),
+    ([Interval(1, 2), Interval(3, 4), Interval(8, 8)], [Interval(1, 2), Interval(3, 4), Interval(8, 8)])
 ])
 def test_merge_intervals(intervals, expected):
     assert merge_intervals(intervals) == expected
