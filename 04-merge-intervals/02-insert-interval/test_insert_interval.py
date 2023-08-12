887bb837955525b3878f8ed736844cf887b299e6
Ben Grass <bvgrass@outlook.com>
2023-08-12 16:36:59 -0400

    added test cases

--- 04-merge-intervals/02-insert-interval/test_insert_interval.py
+++ 04-merge-intervals/02-insert-interval/test_insert_interval.py
@@ -3,6 +3,11 @@ from insert_interval import insert_interval, Interval
 
 
 @pytest.mark.parametrize("intervals, new_interval, expected", [
+    ([[1, 2], [3, 4], [5, 8], [9, 15]], [2, 5], [[1, 8], [9, 15]]),
+    ([[1, 6], [8, 9], [10, 15], [16, 18]], [9, 10], [[1, 6], [8, 15], [16, 18]]),
+    ([[1, 2], [3, 4], [5, 8], [9, 15]], [16, 17], [[1, 2], [3, 4], [5, 8], [9, 15], [16, 17]]),
+    ([[1, 4], [5, 6], [7, 8], [9, 10]], [1, 5], [[1, 6], [7, 8], [9, 10]]),
+    ([[1, 3], [4, 6], [7, 8], [9, 10]], [1, 10], [[1, 10]])
 
 ])
 def test_insert_interval(intervals, new_interval, expected):
