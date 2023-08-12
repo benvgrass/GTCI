63fe7007c272802d84c90fd1ecf160201c8cd1fa
Ben Grass <bvgrass@outlook.com>
2023-08-11 22:19:13 -0400

    fixed single use of list instead of interval in tests

--- 04-merge-intervals/01-merge-intervals/test_merge_intervals.py
+++ 04-merge-intervals/01-merge-intervals/test_merge_intervals.py
@@ -5,7 +5,7 @@ from merge_intervals import Interval
 
 @pytest.mark.parametrize("intervals, expected", [
     ([Interval(1, 5), Interval(3, 7), Interval(4, 6)], [Interval(1, 7)]),
-    ([Interval(1, 5), Interval(4, 6), Interval(6, 8), Interval(11, 15)], [Interval(1, 8), [11, 15]]),
+    ([Interval(1, 5), Interval(4, 6), Interval(6, 8), Interval(11, 15)], [Interval(1, 8), Interval(11, 15)]),
     ([Interval(1, 5)], [Interval(1, 5)]),
     ([Interval(1, 9), Interval(3, 8), Interval(4, 4)], [Interval(1, 9)]),
     ([Interval(1, 2), Interval(3, 4), Interval(8, 8)], [Interval(1, 2), Interval(3, 4), Interval(8, 8)])
