db5f205982e9ccd8591878033661d1f8d9cb376a
Ben Grass <bvgrass@outlook.com>
2023-08-13 13:52:49 -0400

    fixed logic errors using debugging and testing

--- 04-merge-intervals/02-insert-interval/insert_interval.py
+++ 04-merge-intervals/02-insert-interval/insert_interval.py
@@ -5,18 +5,15 @@ def insert_interval(existing_intervals, new_interval):
     existing_count = len(existing_intervals)
 
     # search for place to put new interval
-    while interval_index < existing_count and not new_interval.start > existing_intervals[interval_index]:
+    while interval_index < existing_count and new_interval.start > existing_intervals[interval_index].start:
         new_schedule.append(existing_intervals[interval_index])
         interval_index += 1
 
     # check for overlap with last interval
-    next_interval: Interval
-    if interval_index > 0 and new_schedule[-1].end <= new_interval.start:
+    next_interval = new_interval
+    if interval_index > 0 and new_schedule[-1].end >= new_interval.start:
         last_interval = new_schedule.pop()
-
-        next_interval = Interval(last_interval, max(new_interval.end, last_interval.end))
-    else:
-        next_interval = new_interval
+        next_interval = Interval(last_interval.start, max(new_interval.end, last_interval.end))
 
     # check for overlap with remaining intervals and use it to create the next interval
     # stop looping when there's no more overlap
