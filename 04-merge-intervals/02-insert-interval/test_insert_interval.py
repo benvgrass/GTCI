2328c44c96727d977115557c8122cfeeffa3ff8a
Ben Grass <bvgrass@outlook.com>
2023-08-13 13:26:23 -0400

    fixed error in test method

--- 04-merge-intervals/02-insert-interval/test_insert_interval.py
+++ 04-merge-intervals/02-insert-interval/test_insert_interval.py
@@ -12,5 +12,4 @@ from insert_interval import insert_interval, Interval
 ])
 def test_insert_interval(intervals, new_interval, expected):
     assert insert_interval([Interval(x[0], x[1]) for x in intervals],
-                           Interval(new_interval[0], new_interval[1]) ==
-                           [Interval(e[0], e[1]) for e in expected])
+                           Interval(new_interval[0], new_interval[1])) == [Interval(e[0], e[1]) for e in expected]
