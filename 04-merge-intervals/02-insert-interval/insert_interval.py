41d7b3121222324e4280fd31465bdac2de6d2285
Ben Grass <bvgrass@outlook.com>
2023-08-13 13:21:59 -0400

    implemented next steps of algorithm

--- 04-merge-intervals/02-insert-interval/insert_interval.py
+++ 04-merge-intervals/02-insert-interval/insert_interval.py
@@ -2,17 +2,33 @@ def insert_interval(existing_intervals, new_interval):
     new_schedule = []
     interval_index = 0
 
+    existing_count = len(existing_intervals)
+
     # search for place to put new interval
-    while interval_index < len(existing_intervals) and not new_interval.start > existing_intervals[interval_index]:
+    while interval_index < existing_count and not new_interval.start > existing_intervals[interval_index]:
         new_schedule.append(existing_intervals[interval_index])
         interval_index += 1
 
     # check for overlap with last interval
+    next_interval: Interval
+    if interval_index > 0 and new_schedule[-1].end <= new_interval.start:
+        last_interval = new_schedule.pop()
 
-    # check for overlap with
+        next_interval = Interval(last_interval, max(new_interval.end, last_interval.end))
+    else:
+        next_interval = new_interval
 
+    # check for overlap with remaining intervals and use it to create the next interval
+    # stop looping when there's no more overlap
+    while interval_index < existing_count and next_interval.end >= existing_intervals[interval_index].start:
+        next_interval = Interval(next_interval.start, max(next_interval.end, existing_intervals[interval_index].end))
+        interval_index += 1
+    new_schedule.append(next_interval)
 
-    return []
+    for interval in existing_intervals[interval_index:]:
+        new_schedule.append(interval)
+
+    return new_schedule
 
 
 class Interval:
