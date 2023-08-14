01d1a50c11e332314a85c2a3c4c461a7ddfc26bb
Ben Grass <bvgrass@outlook.com>
2023-08-14 15:38:51 -0400

    implemented test_employee_free_time

--- n04_merge_intervals/n04_free_time/test_free_time.py
+++ n04_merge_intervals/n04_free_time/test_free_time.py
@@ -1,8 +1,11 @@
 import pytest
+from free_time import employee_free_time
+from n04_merge_intervals.interval import make_interval_list, unpack_interval_list
 
 
 @pytest.mark.parametrize("s, expected", [
 
 ])
 def test_employee_free_time(s, expected):
-    assert False
+    assert map(unpack_interval_list,
+               employee_free_time(map(make_interval_list, s))) == expected
