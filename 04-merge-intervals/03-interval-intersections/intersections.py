a52a7e9a9eaf4acc8e5c64b6b5249426266cd420
Ben Grass <bvgrass@outlook.com>
2023-08-14 12:26:58 -0400

    implemented intervals_intersection algorithm in intersections.py

--- 04-merge-intervals/03-interval-intersections/intersections.py
+++ 04-merge-intervals/03-interval-intersections/intersections.py
@@ -1,7 +1,24 @@
 def intervals_intersection(interval_list_a, interval_list_b):
-    # Replace this placeholder return statement with your code
+    result = []
+    a_index = 0
+    b_index = 0
+    len_a = len(interval_list_a)
+    len_b = len(interval_list_b)
 
-    return []
+    while a_index < len_a and b_index < len_b:
+        current_interval_a = interval_list_a[a_index]
+        current_interval_b = interval_list_b[b_index]
+        next_intersection = get_intersection(current_interval_a,
+                                             current_interval_b)
+        if next_intersection:
+            result.append(next_intersection)
+
+        if current_interval_a.end < current_interval_b.end:
+            a_index += 1
+        else:
+            b_index += 1
+
+    return result
 
 
 def get_intersection(interval_a, interval_b):
