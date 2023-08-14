de4121f3795097ac3726cbc8f271304230f7fe67
Ben Grass <bvgrass@outlook.com>
2023-08-14 10:26:47 -0400

    added placeholder for intersections.py

---
+++ 04-merge-intervals/03-interval-intersections/intersections.py
@@ -0,0 +1,24 @@
+def intervals_intersection(interval_list_a, interval_list_b):
+    # Replace this placeholder return statement with your code
+
+    return []
+
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.closed = True  # by default, the interval is closed
+
+        # set the flag for closed/open
+
+    def __eq__(self, other):
+        return isinstance(other, Interval) and other.start == self.start and other.end == self.end
+
+    def set_closed(self, closed):
+        self.closed = closed
+
+    def __str__(self):
+        return "[" + str(self.start) + ", " + str(self.end) + "]" \
+            if self.closed else \
+            "(" + str(self.start) + ", " + str(self.end) + ")"
