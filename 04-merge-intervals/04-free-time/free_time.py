ab8425d177993b36ac554237fff2c64070c65d09
Ben Grass <bvgrass@outlook.com>
2023-08-14 12:45:05 -0400

    adding placeholder code for 04-free-time

---
+++ 04-merge-intervals/04-free-time/free_time.py
@@ -0,0 +1,5 @@
+def employee_free_time(schedule):
+    # Replace this placeholder return statement with your code
+
+    result = []
+    return result
---
+++ 04-merge-intervals/04-free-time/test_free_time.py
@@ -0,0 +1,2 @@
+def test_employee_free_time():
+    assert False
