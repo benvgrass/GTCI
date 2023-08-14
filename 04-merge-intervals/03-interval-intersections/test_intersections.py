c09c5ff28dcaad66c1ab399b668e222b94de88b2
Ben Grass <bvgrass@outlook.com>
2023-08-14 12:32:50 -0400

    fixed test incorrect expected result error in test_intersections.py

--- 04-merge-intervals/03-interval-intersections/test_intersections.py
+++ 04-merge-intervals/03-interval-intersections/test_intersections.py
@@ -8,7 +8,7 @@ from intersections import intervals_intersection, Interval
      [[2, 4], [5, 6], [7, 7], [9, 15]]),
     ([[1, 3], [4, 6], [8, 10], [11, 15]],
      [[2, 3], [10, 15]],
-     [[2, 3], [4, 6], [10, 10], [11, 15]]),
+     [[2, 3], [10, 10], [11, 15]]),
     ([[1, 2], [4, 6], [7, 8], [9, 10]],
      [[3, 6], [7, 8], [9, 10]],
      [[4, 6], [7, 8], [9, 10]]),
@@ -21,9 +21,14 @@ from intersections import intervals_intersection, Interval
 
 ])
 def test_intervals_intersection(a, b, expected):
-    assert intervals_intersection(make_interval_list(a),
-                                  make_interval_list(b)) == make_interval_list(expected)
+    assert unpack_interval_list(
+        intervals_intersection(make_interval_list(a),
+                               make_interval_list(b))) == expected
 
 
 def make_interval_list(l):
     return [Interval(x[0], x[1]) for x in l]
+
+
+def unpack_interval_list(l):
+    return [[x.start, x.end] for x in l]
