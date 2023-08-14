68a4a5f1f7a8d6592667b323805970e80cfedea3
Ben Grass <bvgrass@outlook.com>
2023-08-14 14:14:29 -0400

    fixed package names in one remaining folder

--- n04_merge_intervals/n04_free_time/test_free_time.py
+++ n04_merge_intervals/n04_free_time/test_free_time.py
@@ -1,6 +1,6 @@
 import pytest
 from free_time import employee_free_time
-from .. interval import Interval
+from n04_merge_intervals.interval import Interval
 
 def test_employee_free_time(schedule, expected):
     assert False
