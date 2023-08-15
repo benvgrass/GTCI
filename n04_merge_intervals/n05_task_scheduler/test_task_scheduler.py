5ac0c96ee2dd0166d557f036201aab2fd10f5d38
Ben Grass <bvgrass@outlook.com>
2023-08-15 11:08:55 -0400

    added test cases

--- n04_merge_intervals/n05_task_scheduler/test_task_scheduler.py
+++ n04_merge_intervals/n05_task_scheduler/test_task_scheduler.py
@@ -1,9 +1,14 @@
-from task_scheduler import least_time
+from n04_merge_intervals.n05_task_scheduler.task_scheduler import least_time
 import pytest
 
 
-@pytest.mark.parametrize("tasks,expected", [
-
+@pytest.mark.parametrize("tasks, n,expected", [
+    ([["A", "A", "B", "B"] , 2,5]),
+    ([["A", "A", "A", "B", "B", "C", "C"], 1,7]),
+    ([["S", "I", "V", "U", "W", "D", "U", "X"] , 0,8]),
+    ([["A", "K", "X", "M", "W", "D", "X", "B", "D", "C", "O", "Z", "D", "E", "Q"] , 3,15]),
+    ([["A", "B", "C", "O", "Q", "C", "Z", "O", "X", "C", "W", "Q", "Z", "B", "M", "N", "R", "L", "C", "J"],
+      10, 34])
 ])
-def test_least_time(tasks, expected):
-    assert least_time(tasks) == expected
+def test_least_time(tasks, n, expected):
+    assert least_time(tasks, n) == expected
