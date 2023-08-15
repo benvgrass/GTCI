b077d351b83ca2e90bbe8a0d119d71da561456d2
Ben Grass <bvgrass@outlook.com>
2023-08-14 21:08:38 -0400

    implemented algorithm, kinda

--- n04_merge_intervals/n05_task_scheduler/task_scheduler.py
+++ n04_merge_intervals/n05_task_scheduler/task_scheduler.py
@@ -1,3 +1,11 @@
+from collections import Counter
+
+
 def least_time(tasks, n):
-    # Replace this placeholder return statement with your code
-    return -1
+    wait_time = 0
+    counts = list(Counter(tasks).values()).sort(reverse=True)
+    while counts:
+        wait_time += max(0, n - len(counts))
+        counts = [x - 1 for x in counts if x - 1 > 0]
+
+    return wait_time + len(tasks)
