8b1ae6c652514dd50607518ce8bf2faab2a2336a
Ben Grass <bvgrass@outlook.com>
2023-08-15 14:38:22 -0400

    added test cases for reverse_kgroup.py

--- n05_ll_reversal/n02_reverse_nodes_kgroup/test_reverse_kgroup.py
+++ n05_ll_reversal/n02_reverse_nodes_kgroup/test_reverse_kgroup.py
@@ -2,10 +2,15 @@ from linked_list import LinkedList, linked_list_to_list
 from n05_ll_reversal.n02_reverse_nodes_kgroup.reverse_kgroup import reverse_k_groups
 import pytest
 
-@pytest.mark.parametrize("lst,k,expected", ([
 
+@pytest.mark.parametrize("lst,k,expected", ([
+    ([1, 2, 3, 4, 5], 2, [2, 1, 4, 3, 5]),
+    ([3, 4, 5, 6, 2, 8, 7, 7], 3, [5, 4, 3, 8, 2, 6, 7, 7]),
+    ([1, 2, 3, 4, 5], 1, [1, 2, 3, 4, 5]),
+    ([1, 2, 3, 4, 5, 6, 7], 3, [3, 2, 1, 6, 5, 4, 7]),
+    ([1, 2, 3, 4, 5, 6, 7], 7, [7, 6, 5, 4, 3, 2, 1])
 ]))
-def test_reverse_k_groups(lst,k,expected):
+def test_reverse_k_groups(lst, k, expected):
     ll = LinkedList()
     ll.create_linked_list(lst)
     result = linked_list_to_list(reverse_k_groups(ll.head, k))
