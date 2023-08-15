7857305400ed7132318366e8be7ad1ab2a614e22
Ben Grass <bvgrass@outlook.com>
2023-08-15 15:32:07 -0400

    implemented reverse_between algorithm

--- n05_ll_reversal/n03_reverse_list_2/reverse-list-2.py
+++ n05_ll_reversal/n03_reverse_list_2/reverse-list-2.py
@@ -1,6 +1,19 @@
 from linked_list_node import LinkedListNode
 from n05_ll_reversal.n01_reverse_list.reverse_list import reverse_linked_list
+from n05_ll_reversal.n02_reverse_nodes_kgroup.reverse_kgroup import traverse_to_depth_k
+
+
 def reverse_between(head, left, right):
-    # Replace this placeholder return statement with your code
+    left_node = traverse_to_depth_k(head, left)
+    right_node = traverse_to_depth_k(head, right)
+    resume_node = right_node.next
+    right_node.next = None
+    reverse_linked_list(left_node)
+    left_node.next = resume_node
+    if left > 1:
+        pause_node = traverse_to_depth_k(head, left - 1)
+        pause_node.next = right_node
+    else:
+        head = right
 
     return head
