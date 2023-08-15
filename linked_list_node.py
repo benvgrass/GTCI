2f51c10cb42de00e887933ca8124175aae5030b2
Ben Grass <bvgrass@outlook.com>
2023-08-15 12:18:43 -0400

    add linked_list.py and linked_list_node.py and associated classes

---
+++ linked_list_node.py
@@ -0,0 +1,7 @@
+# from educative.io (with some changes)
+
+class LinkedListNode:
+    # __init__ will be used to make a LinkedListNode type object.
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
