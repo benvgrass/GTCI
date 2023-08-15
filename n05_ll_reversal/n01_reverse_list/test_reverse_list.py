f60030f4a97a7e8facb17fc805e07841669742ba
Ben Grass <bvgrass@outlook.com>
2023-08-15 13:34:46 -0400

    implemented test

---
+++ n05_ll_reversal/n01_reverse_list/test_reverse_list.py
@@ -0,0 +1,18 @@
+import pytest
+from n05_ll_reversal.n01_reverse_list.reverse_list import reverse
+from linked_list import LinkedList, linked_list_to_list
+
+
+@pytest.mark.parametrize("lst", ([
+    [1, -2, 3, 4, -5, 4, 3, -2, 1],
+    [-1, -5, -3, -7, -8, -6, -2],
+    [-1, 2, -3, 4],
+    [1, -1, -2, 3, -4, 5],
+    [28, 21, 14, 7]
+]))
+def test_reverse(lst):
+    ll = LinkedList()
+    ll.create_linked_list(lst)
+    reversed_ll = LinkedList()
+    reversed_ll.insert_node_at_head(reverse(ll.head))
+    assert linked_list_to_list(reversed_ll) == lst.reverse()
